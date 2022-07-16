677e62d4cbff086c6ebf8c5eb8392fed3bad3003
ghannesh59 <92075482+ghannesh59@users.noreply.github.com>
2022-07-16 16:10:22 -0700

    Time: 137 ms (67.98%), Space: 17.1 MB (96.43%) - LeetHub

---
+++ 49-group-anagrams/49-group-anagrams.py
@@ -0,0 +1,13 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        output=[]
+        anagram_dict={}
+        for i in strs:
+            val=''.join(sorted(i))
+            if val not in anagram_dict:
+                anagram_dict[val]=[i]
+            else:
+                anagram_dict[val].append(i)
+        for i in anagram_dict:
+            output.append(anagram_dict[i])
+        return output
