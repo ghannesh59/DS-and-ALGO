dcc79158b48dc8f76565c64cae00abb458d58b4e
ghannesh59 <92075482+ghannesh59@users.noreply.github.com>
2022-08-01 10:08:30 -0700

    Time: 204 ms (33.22%), Space: 17.6 MB (73.84%) - LeetHub

--- 23-merge-k-sorted-lists/23-merge-k-sorted-lists.py
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.py
@@ -1,26 +1,47 @@
 # Definition for singly-linked list.
-# class ListNode(object):
+# class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution(object):
-    def mergeKLists(self, lists):
-        """
-        :type lists: List[ListNode]
-        :rtype: ListNode
-        """
-        heap=[]
-        curr=dummy=ListNode(0)
-        for l in lists:
-            if l:
-                heapq.heappush(heap,(l.val,l))
-        while(len(heap)>0):
-            val,node=heapq.heappop(heap)
-            curr.next=ListNode(val)
-            curr=curr.next
-            node=node.next
-            if node:
-                heapq.heappush(heap,(node.val,node))
+class Solution:
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+#         res=[]
+#         dummy=curr=ListNode(0)
+#         for i in range(len(lists)):
+#             while(lists[i]!=None):
+#                 res.append(lists[i].val)
+#                 lists[i]=lists[i].next
+#         res=sorted(res)
+#         for i in res:
+#             dummy.next=ListNode(i)
+#             dummy=dummy.next
+#         dummy.next=None
+#         return curr.next
+            if len(lists)==0:
+                return None
+        
+            while(len(lists)>1):
+                merged_lists=[]
+                for i in range(0,len(lists),2):
+                    l1=lists[i]
+                    l2=lists[i+1] if (i+1)<len(lists) else None
+                    merged_lists.append(self.merge(l1,l2))
+                lists=merged_lists
+            return lists[0]
+    
+    def merge(self,l1,l2):
+        dummy=curr=ListNode(0)
+        while l1 and l2:
+            if l1.val>l2.val:
+                curr.next=l2
+                curr=curr.next
+                l2=l2.next
+            else:
+                curr.next=l1
+                curr=curr.next
+                l1=l1.next
+        if l1:
+            curr.next=l1
+        else:
+            curr.next=l2
         return dummy.next
-            
-            
