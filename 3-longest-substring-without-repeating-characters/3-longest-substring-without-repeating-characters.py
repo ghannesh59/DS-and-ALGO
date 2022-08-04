35885ea08a9f52bd9b81dcc42090ed5134e10639
ghannesh59 <92075482+ghannesh59@users.noreply.github.com>
2022-08-04 08:31:18 -0700

    Time: 63 ms (73.38%), Space: 14.7 MB (10.00%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.py
@@ -1,18 +1,31 @@
-class Solution:
-    def lengthOfLongestSubstring(self, s: str) -> int:
-        start=0
-        res=0
-        seen={}
-        for end in range(len(s)):
-            if s[end] not in seen:
-                res=max(res,(end-start)+1)
-            else:
-                if seen[s[end]]<start:
-                    res=max(res,(end-start)+1)
-                else:
-                    start=seen[s[end]]+1
-            seen[s[end]]=end
-        return res
-
-
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
                 
+        """
+        :type s: str
+        :rtype: int
+        """
+        max1=0
+        start=0
+        end=0
+        dict1={}
+        set1=set(s)
+        if len(set1)==len(s):
+            return len(s)
+        elif len(s)==0:
+            return 0
+        else:
+            for i in range(len(s)):
+                if s[i] in dict1 and dict1[s[i]]>=start:
+                        end=end+1
+                        start=dict1[s[i]]+1
+                        len2=end-start
+                        if len2>max1:
+                            max1=len2    
+                else:
+                    end=end+1
+                    len3=end-start
+                    if len3>max1:
+                        max1=len3
+                dict1[s[i]]=i
+            return max1
