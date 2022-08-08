270515a985ea3c76152373f25f257ac5d6145ace
ghannesh59 <92075482+ghannesh59@users.noreply.github.com>
2022-08-08 08:23:37 -0700

    Time: 467 ms (42.29%), Space: 30.5 MB (36.94%) - LeetHub

---
+++ 200-number-of-islands/200-number-of-islands.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def numIslands(self, grid):
+        """
+        :type grid: List[List[str]]
+        :rtype: int
+        """
+        count=0
+        rows,cols=len(grid),len(grid[0])
+        visited=[[False for i in range(cols)] for j in range(rows)]
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c]=="1" and visited[r][c]==False:
+                    self.dfs(r,c,grid,visited)
+                    count=count+1
+        return count
+    def dfs(self,i,j,grid,visited):
+        if i<0 or j<0 or i>=len(grid) or j>=len(grid[0]) or grid[i][j]=="0" or visited[i][j]==True:
+            return
+        visited[i][j]=True
+        self.dfs(i+1,j,grid,visited)
+        self.dfs(i-1,j,grid,visited)
+        self.dfs(i,j+1,grid,visited)
+        self.dfs(i,j-1,grid,visited)
