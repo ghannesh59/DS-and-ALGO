12a18ac8882469d3cf6db2d10481c92131f8ed51
ghannesh59 <92075482+ghannesh59@users.noreply.github.com>
2022-08-17 03:26:04 -0700

    Time: 497 ms (36.72%), Space: 30.4 MB (39.29%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.py
+++ 200-number-of-islands/200-number-of-islands.py
@@ -1,22 +1,21 @@
-class Solution:
-    def numIslands(self, grid: List[List[str]]) -> int:
-        if len(grid)==0:
-            return 0
+class Solution(object):
+    def numIslands(self, grid):
+        """
+        :type grid: List[List[str]]
+        :rtype: int
+        """
         count=0
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
-                if grid[i][j]=="1":
-                    grid[i][j]=0
-                    self.bfs(grid,i,j)
+        rows,cols=len(grid),len(grid[0])
+        visited=[[False for i in range(cols)] for j in range(rows)]
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c]=="1" and visited[r][c]==False:
+                    self.dfs(r,c,grid,visited)
                     count=count+1
         return count
-    def bfs(self,grid,i,j):
-        queue=[]
-        queue.append((i,j))
-        while(queue):
-            i,j=queue.pop(0)
-            for i,j in (i+1,j),(i-1,j),(i,j+1),(i,j-1):
-                if 0<=i<len(grid) and 0<=j<len(grid[0]) and grid[i][j]=="1":
-                    queue.append((i,j))
-                    grid[i][j]=0
-        
+    def dfs(self,i,j,grid,visited):
+        if i<0 or j<0 or i>=len(grid) or j>=len(grid[0]) or grid[i][j]=="0" or visited[i][j]==True:
+            return
+        visited[i][j]=True
+        for i,j in (i+1,j),(i-1,j),(i,j+1),(i,j-1):
+            self.dfs(i,j,grid,visited)
