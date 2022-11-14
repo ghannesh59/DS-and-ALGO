17326a7ecd1815051ff29fa377e5ec4c3717525d
ghannesh59 <92075482+ghannesh59@users.noreply.github.com>
2022-11-14 09:36:15 -0800

    Time: 135 ms (29.01%), Space: 13.4 MB (91.65%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.py
+++ 0733-flood-fill/0733-flood-fill.py
@@ -6,19 +6,20 @@ class Solution(object):
         :type color: int
         :rtype: List[List[int]]
         """            
-        def dfs_helper(self,image,visited,sr,sc,color,original_value):
-            if sr>=len(image) or sc>=len(image[0]) or sr<0 or sc<0 or visited[sr][sc]==True or image[sr][sc]!=original_value:
+        def dfs_helper(self,image,sr,sc,color,original_value):
+            if sr>=len(image) or sc>=len(image[0]) or sr<0 or sc<0 or image[sr][sc]!=original_value:
                 return
-            if visited[sr][sc]==False:
-                visited[sr][sc]=True
-                if image[sr][sc]==original_value:
-                    image[sr][sc]=color
-            self.dfs_helper(image,visited,sr+1,sc,color,original_value)
-            self.dfs_helper(image,visited,sr,sc+1,color,original_value)
-            self.dfs_helper(image,visited,sr-1,sc,color,original_value)
-            self.dfs_helper(image,visited,sr,sc-1,color,original_value)
+            # if visited[sr][sc]==False:
+            #     visited[sr][sc]=True
+            if image[sr][sc]==original_value:
+                image[sr][sc]=color
+            self.dfs_helper(image,sr+1,sc,color,original_value)
+            self.dfs_helper(image,sr,sc+1,color,original_value)
+            self.dfs_helper(image,sr-1,sc,color,original_value)
+            self.dfs_helper(image,sr,sc-1,color,original_value)
         def floodFill(self, image, sr, sc, color):
-            visited=[[False for i in range(len(image[0]))]for i in range(len(image))]
+            #visited=[[False for i in range(len(image[0]))]for i in range(len(image))]
             original_value=image[sr][sc]
-            self.dfs_helper(image,visited,sr,sc,color,original_value)
+            if color == original_value: return image
+            self.dfs_helper(image,sr,sc,color,original_value)
             return image
